fbce6b02babe5294d601591172272bbda725e362
Elvis Lima <elvisdelima89@gmail.com>
2013-01-31 21:24:02 -0200

    commit inicial

---
+++ Models/Tarefas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kambao.Models
+{
+    public class Tarefas
+    {
+        public virtual long Id { get; set; }
+        public virtual string Codigo { get; set; }
+        public virtual string Descricao { get; set; }
+    }
+}
---
+++ Scripts/cardcontrol.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.cartao').draggable({
+        cursor: 'move',          // sets the cursor apperance
+        opacity: 0.70            // opacity while the element is dragged
+    });
+});
