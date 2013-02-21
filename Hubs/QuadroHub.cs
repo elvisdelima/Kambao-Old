9e2b508c93e0dbc3655b6ba38f7cfc2592d46ec9
Elvis Lima <elvisdelima89@gmail.com>
2013-02-21 17:49:55 -0300

    vai tartaruguinha 2!

--- Hubs/QuadroHub.cs
+++ Hubs/QuadroHub.cs
@@ -3,12 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Kambao.Controllers;
+using Kambao.Data;
+using Kambao.Core;
 
 namespace Kambao.Hubs
 {
     public class QuadroHub : Hub
     {
-        public void MoverCartao() {
+        public void moveCard(int idTarefa, int idFaixa) {
+            CrudData<Tarefa> TarefasData = new CrudData<Tarefa>();
+            Tarefa tarefa = TarefasData.Get(idTarefa);
+
+            CrudData<Faixa> FaixasData = new CrudData<Faixa>();
+            Faixa faixa = FaixasData.Get(idFaixa);
+            
+            Clients.All.alterarFaixaDoCartao(tarefa, faixa);        
         }
-    }
+    }    
 }
