49f2557ee4891104d38af6bcce4011fc7fb71d7d
Elvis Lima <elvisdelima89@gmail.com>
2013-05-03 17:16:40 -0300

    SignalR movendo cartões em todos os browsers

--- Hubs/QuadroHub.cs
+++ Hubs/QuadroHub.cs
@@ -3,22 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Kambao.Controllers;
-using Kambao.Data;
-using Kambao.Core;
 
 namespace Kambao.Hubs
 {
     public class QuadroHub : Hub
     {
-        public void moveCard(int idTarefa, int idFaixa) {
-            CrudData<Tarefa> TarefasData = new CrudData<Tarefa>();
-            Tarefa tarefa = TarefasData.Get(idTarefa);
-
-            CrudData<Faixa> FaixasData = new CrudData<Faixa>();
-            Faixa faixa = FaixasData.Get(idFaixa);
-            
-            Clients.All.alterarFaixaDoCartao(tarefa, faixa);        
+        public void MoveCard(string idTarefa, string idFaixa) 
+        {
+            Clients.All.alterarFaixaDoCartao(idTarefa, idFaixa);        
         }
     }    
 }
